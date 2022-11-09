11bd0dfad652e0116edb517e1bce80a167c94522
rubenaprikyan <rubenaprikyan@gmail.com>
2022-11-10 01:28:30 +0400

    Add scrapper for physic posts

---
+++ README.md
@@ -0,0 +1,9 @@
+### Scrapper
+
+_How to execute scrapper ?_
+```shell
+  pip install scrapy bs4
+```
+```shell
+  scrapy runspider ./scrapper/physics_scrapper.py -o content.csv -t csv
+```
---
+++ scrapper/physics_scrapper.py
@@ -0,0 +1,43 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+
+class ContentItem(scrapy.Item):
+    title = scrapy.Field()
+    content = scrapy.Field()
+
+
+class PhysicsPostSpider(scrapy.Spider):
+    name = "posts"
+
+    def start_requests(self):
+        urls = ["https://www.weltderphysik.de/service/suche"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        count = 0
+        for item in response.css('.teaser-text'):
+            post = [item.css('a::text').get(), item.css('a::attr(href)').get()]
+            count += 1
+            yield self.request_page(link=post[1], title=post[0])
+
+    def request_page(self, link, title):
+        return scrapy.Request(
+            "https://www.weltderphysik.de" + link,
+            callback=self.parse_page,
+            cb_kwargs=dict(title=title)
+        )
+
+    def parse_page(self, response, title):
+        text = self.stripText(response.css(".ce-bodytext"))
+        item = ContentItem()
+        item['title'] = title
+        item['content'] = text
+
+        return item
+
+    @staticmethod
+    def stripText(selector):
+        soup = BeautifulSoup(selector.get())
+        return soup.get_text().strip()
